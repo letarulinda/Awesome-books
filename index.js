e81c646e466667c6e69314bb24acc538da305d6f
letarulinda <letarulinda@gmail.com>
2022-10-07 12:23:28 -0700

    fix linters error

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ form.addEventListener('submit', (e) => {
 
   // validating form fields
   if (title === '' || author === '') {
+    return;
   } else {
     const book = new Book(title, author);
     awesomeHeading.style.display = 'block';
@@ -40,6 +41,8 @@ form.addEventListener('submit', (e) => {
     displayBooksControl.clearFields();
   }
 });
+
+
 // remove the book
 document.querySelector('#book-list').addEventListener('click', (e) => {
   displayBooksControl.deleteBook(e.target);
